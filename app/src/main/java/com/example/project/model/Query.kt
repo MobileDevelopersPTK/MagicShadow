562c3d45e79e3e4c947585fd21bab1f268d32424
AS <as@gmail.com>
2022-07-11 13:54:12 +0300

    retrofit add registration

--- app/src/main/java/com/example/project/model/Query.kt
+++ app/src/main/java/com/example/project/model/Query.kt
@@ -1,6 +1,7 @@
 package com.example.project.model
 
 import com.example.project.model.api.data.Auth
+import com.example.project.model.api.data.Registration
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -12,5 +13,10 @@ interface Query {
     @POST("user/authorization.php")
     @Headers("Content-Type: application/json")
     fun checkAuthUser() : Call<ArrayList<Auth>>
+
+    // Регистрация
+    @POST("user/registration.php")
+    @Headers("Content-Type: application/json")
+    fun registration() : Call<ArrayList<Registration>>
 
 }
--- app/src/main/java/com/example/project/view_model/MainViewModel.kt
+++ app/src/main/java/com/example/project/view_model/MainViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.project.model.api.data.Auth
+import com.example.project.model.api.data.Registration
 import com.example.project.model.api.data.RetrofitInstance.Companion.service
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,6 +13,8 @@ import java.util.ArrayList
 
 class MainViewModel : ViewModel() {
     val auth: MutableLiveData<ArrayList<Auth>> = MutableLiveData()
+    val registration: MutableLiveData<ArrayList<Registration>> = MutableLiveData()
+
     fun getProducts(number: String) {
         when (number) {
             "auth" ->
@@ -25,6 +28,22 @@ class MainViewModel : ViewModel() {
                     }
 
                     override fun onFailure(call: Call<ArrayList<Auth>>, t: Throwable) {
+                        Log.i("TAG", "Не удачно $call $t")
+                    }
+                })
+            }
+
+            "registration" ->
+            {
+                service.registration().enqueue(object : Callback<ArrayList<Registration>> {
+                    override fun onResponse(
+                        call: Call<ArrayList<Registration>>,
+                        response: Response<ArrayList<Registration>>
+                    ) {
+                        if (response.isSuccessful) registration.value = response.body()
+                    }
+
+                    override fun onFailure(call: Call<ArrayList<Registration>>, t: Throwable) {
                         Log.i("TAG", "Не удачно $call $t")
                     }
                 })
